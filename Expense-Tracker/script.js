30eb597e1a5491423e40c0aedeceda16c036865f
Henry Tan <dragonninja79@gmail.com>
2026-03-23 16:53:00 +0800

    import python

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -53,11 +53,12 @@ function addTransactionDOM(transaction) {
 //update balance
 function updateValues(){
 const amounts=transactions.map(transaction=>transaction.amount);
-const total=amounts.reduce((acc,item)=>(acc+=item),0).toFixed(2);
+
+const total=amounts.reduce((acc,item)=>acc+=item,0).toFixed(2);
 const income=amounts.filter(item=>item>0)
-                    .reduce((acc,item)=>(acc+=item),0).toFixed(2);
+                    .reduce((acc,item)=>acc+=item,0).toFixed(2);
 const expense=(amounts.filter(item=>item<0)
-                    .reduce((acc,item)=>(acc+=item),0)*-1).toFixed(2);
+                    .reduce((acc,item)=>acc+=item,0)*-1).toFixed(2);
 
 balance.innerText=`${total}`;
 money_plus.innerText=`${income}`;
---
+++ Movie Seat Booking/index.js
@@ -0,0 +1,60 @@
+const container=document.querySelector(".movie-container");
+const seats=document.querySelectorAll(".row .seat:not(.occupied)");
+const count=document.getElementById("count");
+const total=document.getElementById("total");
+const movieSelect=document.querySelector(".movies");
+
+let ticketPrice=+movieSelect.value;
+
+const updateSelectedCount=()=>{
+    const selectedSeats=document.querySelectorAll(".row .seat.selected");
+  
+    const selectedSeatCount=selectedSeats.length;
+
+    count.innerText=selectedSeatCount;
+    total.innerText=selectedSeatCount*ticketPrice;
+
+    saveData();
+
+}
+
+container.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("seat")&&
+    !e.target.classList.contains("occupied")){
+        e.target.classList.toggle("selected");
+            updateSelectedCount();
+            
+    }
+})
+movieSelect.addEventListener("change",e=>{
+    ticketPrice=+e.target.value;
+    updateSelectedCount();
+   
+})
+const saveData=()=>{
+    const selectedSeats=document.querySelectorAll(".row .seat.selected");
+
+   const seatIndex=[...selectedSeats].map(seat=>[...seats].indexOf(seat));
+
+   const data={
+    seats:seatIndex,
+    movieIndex:movieSelect.selectedIndex
+   }
+   localStorage.setItem("movieData",JSON.stringify(data));
+}
+const getData=()=>{
+const data=JSON.parse(localStorage.getItem("movieData"));
+
+if(data!==null){
+    seats.forEach((seat,index)=>{
+        if(data.seats.includes(index)){
+            seat.classList.add("selected");
+        }
+    })
+    movieSelect.selectedIndex=data.movieIndex;
+    ticketPrice=+movieSelect.value;
+
+}
+}
+getData();
+updateSelectedCount();
